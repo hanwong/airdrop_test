01ddadcfed509fe94fe5b4e930bcf92305502558
hanwong <hanationbear@gmail.com>
2024-10-09 18:15:03 +0900

    Set connecting telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ky from 'ky'
+import { useForm } from 'react-hook-form'
+import { retrieveLaunchParams } from '@telegram-apps/sdk'
+import { useMutation } from '@tanstack/react-query'
 import WebApp from '@twa-dev/sdk'
+import './App.css'
+
+const api = ky.create({ prefixUrl: "https://airdrop-api.devnet.initia.xyz/" })
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { initDataRaw } = retrieveLaunchParams()
+
+  const { register, watch, handleSubmit } = useForm<{ address: string }>({
+    defaultValues: { address: "" },
+  })
+  const { address } = watch()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (address: string) => {
+      const result = await api.post("claim/social", { 
+        body: JSON.stringify({
+          type: 3,
+          receiver: address,
+          code: `tma ${initDataRaw}`
+        })
+      })
+      return JSON.stringify(result)
+    },
+    onSuccess: (response: string) => {
+      WebApp.showAlert(response)
+    },
+    onError: (error: Error) => {
+      WebApp.showAlert(error.message)
+    },
+  })
+
+  const submit = handleSubmit(async ({ address }) => {
+    mutate(address)
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit={submit}>
+      <div className="contents">
+        <p className="title">Welcome, Initiate.</p>
+        <p className="text">
+          <span className="sub-text">
+            To confirm your identity and to connect your wallet, please enter your Initia wallet address below.
+            Please note that you only get one chance to get your address entered. 
+          </span> Make sure it is the correct address, or you will not be eligible for the airdrop.
+          <br />
+          <br />
+          Please make sure to do this on a desktop, as the airdrop website does not support mobile devices.
+        </p>
+        <input 
+          {...register("address", {
+            required: true,
+          })}
+          className="address"
+          type="text" 
+          placeholder="Enter Initia wallet address"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-        {/* Here we add our button with alert callback */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
+      <button type="submit" className="confirm" disabled={!address || isPending}>
+        Confirm
+      </button>
+    </form>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import WebApp from '@twa-dev/sdk'
 import App from './App.tsx'
 import './index.css'
-import WebApp from '@twa-dev/sdk'
 
 WebApp.ready()
+export const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
